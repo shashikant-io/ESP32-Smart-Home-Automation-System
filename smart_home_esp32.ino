aba55460351709f22452a2b1aa22c274c12b1552
Shashikant Kumar <shashikantchilga03@gmail.com>
2026-04-28 23:06:04 +0530

    Include servo control example in smart_home_esp32
    Added example code for controlling a servo motor.

--- smart_home_esp32.ino
+++ smart_home_esp32.ino
@@ -213,3 +213,26 @@ void loop() {
     digitalWrite(WIFI_LED, LOW);
   }
 }
+
+
+
+or you can check your servo by this code 
+#include <Servo.h>
+
+Servo myServo;  // create servo object
+
+void setup() {
+  myServo.attach(9);  // connect servo signal pin to digital pin 9
+}
+
+void loop() {
+  myServo.write(0);   // move to 0°
+  delay(1000);
+
+  myServo.write(90);  // move to 90°
+  delay(1000);
+
+  myServo.write(180); // move to 180°
+  delay(1000);
+}
+
